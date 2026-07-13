6052d0da0f8199eb561a273bfa812dff01068653
Cauanysales <162763663+Cauanysales@users.noreply.github.com>
2026-07-13 20:26:15 +0000

    feat(app): configurar aplicação Flask

---
+++ src/wishlist-app/wishlist/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'wishlist.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    @app.route('/')
+    def index():
+        return 'Wishlist App'
+
+    return app
